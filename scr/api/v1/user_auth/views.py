d280d4ff6d6404ac3074104d9ee7633defdf6984
EnterNick <Ravil.Mirgayazov@yandex.ru>
2024-12-15 01:22:14 +0300

    Add login template, optimize css classes

--- scr/api/v1/user_auth/views.py
+++ scr/api/v1/user_auth/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
 from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -23,22 +25,26 @@ class UserRegistrationView(CreateAPIView):
 
 class LoginView(APIView):
     serializer_class = LoginRequestSerializer
-
     token_generate_serializer = UserObtainTokenSerializer
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'user_auth/login.html'
 
     def post(self, request, **kwargs):
         serializer = self.serializer_class(data=request.data)
 
         if not serializer.is_valid():
-            return Response(data=serializer.errors, status=422)
+            return Response(data={'errors': serializer.errors}, status=422)
 
         token_serializer = self.token_generate_serializer(data=serializer.data)
 
         token_serializer.is_valid(raise_exception=True)
 
-        response = Response(data=token_serializer.data, status=200)
+        response = redirect('all_goods')
 
         response.set_cookie('accessToken', token_serializer.data['access_token'])
         response.set_cookie('refreshToken', token_serializer.data['refresh_token'])
 
         return response
+
+    def get(self, request, *args, **kwargs):
+        return Response(template_name=self.template_name)
