165971b0ce09cc9fc7172182863fa406f300f21a
EnterNick <Ravil.Mirgayazov@yandex.ru>
2024-12-16 22:22:17 +0300

    Add side ber preview in create good template

--- scr/config/static/js/edit-good.js
+++ scr/config/static/js/edit-good.js
@@ -1,49 +1,38 @@
 const price = document.querySelector('#price')
 const prePrice = document.querySelector('.product-page__price-value')
-const prePrices = document.querySelector('.products__product__price-value')
 
 const description = document.querySelector('#description')
 const preDesc = document.querySelector('.product-page__details_description')
-const preDescs = document.querySelector('.products__product__description')
 
 const title = document.querySelector('#title')
 const preTitle = document.querySelector('.product-page__title')
-const preTitles = document.querySelector('.products__product__title')
 
 const quanity = document.querySelector('#value')
 const preQuanity = document.querySelector('.product-page__quantity-value')
-const preQuanity_second = document.querySelector('.products__product__quanity-value')
 
 const category = document.querySelector('#category')
 const preCategory = document.querySelector('.product-page__category-value')
-const preCategories = document.querySelector('.products__product__category-value')
 
 const preImage = document.querySelector('.product-page__image-img')
-const preImages = document.querySelector('.products__product__image')
 
 price.addEventListener('change', function () {
         prePrice.textContent = price.value + ' руб.'
-        prePrices.textContent = price.value + ' руб.'
     }
 )
 title.addEventListener('change', function () {
         preTitle.textContent = title.value
-        preTitles.textContent = title.value
     }
 )
 description.addEventListener('change', function () {
         preDesc.textContent = description.value
-        preDescs.textContent = description.value
     }
 )
 quanity.addEventListener('change', function () {
         preQuanity.textContent = quanity.value
-        preQuanity_second.textContent = quanity.value
     }
 )
 category.addEventListener('change', function () {
         preCategory.textContent = category.value
-        preCategories.textContent = category.value
     }
 )
 
@@ -55,7 +44,6 @@ function imagePreview () {
         'load',
         function () {
             preImage.src = reader.result;
-            preImages.src = reader.result;
         }
     )
     if (image) {
@@ -64,15 +52,10 @@ function imagePreview () {
 }
 
 prePrice.textContent = price.value + ' руб.'
-prePrices.textContent = price.value + ' руб.'
 preTitle.textContent = title.value
-preTitles.textContent = title.value
 preDesc.textContent = description.value
-preDescs.textContent = description.value
 preQuanity.textContent = quanity.value
-preQuanity_second.textContent = quanity.value
 preCategory.textContent = category.value
-preCategories.textContent = category.value
 
 
 const modal = document.querySelector('.modal')
@@ -91,3 +74,17 @@ modalBtnClose.onclick = function () {
     modal.style.display = 'none'
     body.style.overflow = 'scroll'
 }
+
+
+const preview = document.querySelector('.preview')
+preview.addEventListener('click', function () {
+    preview.classList.toggle('preview__activate-full')
+})
+
+preview.addEventListener('mouseover', function () {
+    preview.classList.add('preview__activate')
+})
+
+preview.addEventListener('mouseout', function () {
+    preview.classList.remove('preview__activate')
+})
